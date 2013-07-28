a12fdd88f34ff36daeb3c9fa9843c16582d940e3
Johnathan James <haxwell@gmail.com>
2013-07-28 13:43:23 -0600

    Moved duplicate code to a common method in question-collections.js.. Its cleaner now

--- src/main/webapp/js/collections/question-collections.js
+++ src/main/webapp/js/collections/question-collections.js
@@ -1,3 +1,35 @@
+Collection.Methods = {
+			addUniqueModelToCollection : function(model, modelKeysFunction, quizkiCollection) {
+					var millis = new Date().getMilliseconds();
+					
+					if (modelKeysFunction !== undefined) {
+						var nameOfComparisonKey = modelKeysFunction();
+						
+						if (nameOfComparisonKey.length > 0) {
+							
+							// get all the items in the collection that have an attribute named after the comparison key,
+							//  then, for each of those, return those who have the same value for that attribute as our model does.
+							//  .. the point being if we find a match, we don't add this new model to our collection. No dupes.
+							var v = _.filter(
+										_.filter(quizkiCollection.models, 
+												function(item) { 
+													return 	item !== undefined &&
+															item.attributes.val !== undefined && item.attributes.val[nameOfComparisonKey] !== undefined; 
+												}
+										)
+										, function(item) { 
+											return 	item !== undefined &&
+													item.attributes.val[nameOfComparisonKey] == model[nameOfComparisonKey];
+										}
+									);
+							
+							if (v.length == 0) quizkiCollection.add( [ {val:model, millisecond_id:millis} ] );
+						}
+					}
+					else 
+						quizkiCollection.add([{val:model, millisecond_id:millis}]);
+				}
+};
 
 Quizki.Collection = Backbone.Collection.extend({
 		initialize: function() {
@@ -8,34 +40,10 @@ Quizki.Collection = Backbone.Collection.extend({
 			}
 		},
 		put: function(model, throwEvent) {
+			Collection.Methods.addUniqueModelToCollection(model, this.modelKeysFunction, this);
+			
 			// Created this method put, because I couldn't find a way to override add(), so that I could
 			//  trigger the 'somethingChanged' event when something was added.
-			model.millisecond_id = new Date().getMilliseconds();
-			
-			var millis = new Date().getMilliseconds();
-			
-			if (this.modelKeysFunction !== undefined) {
-				var nameOfComparisonKey = this.modelKeysFunction();
-				
-				if (nameOfComparisonKey.length > 0) {
-					
-					var v = _.filter(
-								_.filter(this.models, 
-										function(item) { 
-											return item.attributes.val !== undefined && item.attributes.val[nameOfComparisonKey] !== undefined; 
-										}
-								)
-								, function(item) { 
-									return item.attributes.val[nameOfComparisonKey] == arr[i][nameOfComparisonKey];
-								}
-							);
-					
-					if (v.length == 0)
-						this.add( [ {val:model, millisecond_id:millis} ] );
-					}
-				}
-				else 
-					this.add([{val:model, millisecond_id:millis}]);
 			
 			if (throwEvent !== false)
 				this.trigger('somethingChanged');
@@ -43,30 +51,8 @@ Quizki.Collection = Backbone.Collection.extend({
 			return model.millisecond_id;     
 		},
 		addArray: function(arr, isThrowEvent) {
-			for (var i=0; i<arr.length; i++) {
-
-				var millis = new Date().getMilliseconds();
-				
-				if (this.modelKeysFunction !== undefined) {
-					var nameOfComparisonKey = this.modelKeysFunction();
-					
-					if (nameOfComparisonKey.length > 0) {
-						
-						var v = _.filter(
-									_.filter(this.models, function(item) { 
-										return item.attributes.val !== undefined && item.attributes.val[nameOfComparisonKey] !== undefined; 
-									})
-									, function(item) { 
-								return item.attributes.val[nameOfComparisonKey] == arr[i][nameOfComparisonKey];
-						});
-						
-						if (v.length == 0)
-							this.add( [ {val:arr[i], millisecond_id:millis} ] );
-						}
-					}
-					else 
-						this.add([{val:arr[i], millisecond_id:millis}]);
-			}
+			for (var i=0; i<arr.length; i++) 
+				Collection.Methods.addUniqueModelToCollection(arr[i], this.modelKeysFunction, this);
 			
 			if (isThrowEvent !== false)
 				this.trigger('somethingChanged');	
@@ -78,10 +64,6 @@ Quizki.Collection = Backbone.Collection.extend({
 			var v = _.filter(this.models, function (item) {return item.attributes.millisecond_id == millis;	})[0];
 			
 			v.attributes.val[attr] = value;
-			
-//			var map = {};
-//			map[attr] = value;
-//			v.set(map);
 			
 			if (throwEvent !== false)
 				this.trigger('somethingChanged'); 

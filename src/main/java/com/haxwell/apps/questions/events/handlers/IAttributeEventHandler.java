e923a20dd21950adec1f8b2855d63dddde3aad4d
Johnathan <haxwell@gmail.com>
2014-02-22 20:15:09 -0700

    Event Handling, server side, just making things a bit more clear, adding comments, setting better variable names, etc

--- src/main/java/com/haxwell/apps/questions/events/handlers/IAttributeEventHandler.java
+++ src/main/java/com/haxwell/apps/questions/events/handlers/IAttributeEventHandler.java
@@ -2,6 +2,11 @@ package com.haxwell.apps.questions.events.handlers;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Defines a class which is called when some event happens.
+ * 
+ * @author jjames
+ */
 public interface IAttributeEventHandler extends IEventHandler {
 
 	public void setAttribute(String attr);

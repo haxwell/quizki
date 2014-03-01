5930bd1771fd5025a4b8950e60c793ee2a342b75
Johnathan <haxwell@gmail.com>
2014-03-01 16:06:09 -0700

    initial commit

---
+++ src/main/webapp/js/choice.js
@@ -0,0 +1,14 @@
+
+var Choice = Backbone.Model.extend({
+	defaults: {
+		id : -1,
+		text : '',
+		iscorrect : false,
+		isselected : false,
+		sequence : 0,
+		string : ''
+	},
+	initialize : function() {
+		
+	}
+});

8a6df4320e42804a19688b619cfbf8a60dbaead6
Johnathan <haxwell@gmail.com>
2014-03-23 09:17:00 -0600

    Do not store AJAX requests

--- src/main/java/com/haxwell/apps/questions/servlets/filters/MostRecentlyRequestedPageFilter.java
+++ src/main/java/com/haxwell/apps/questions/servlets/filters/MostRecentlyRequestedPageFilter.java
@@ -45,7 +45,7 @@ public class MostRecentlyRequestedPageFilter extends AbstractFilter {
 	private boolean thisRequestShouldBeSaved(String url) {
 		String lowercaseurl = url.toLowerCase();
 		
-		return lowercaseurl.endsWith(".jsp") && !lowercaseurl.contains("login"); 
+		return lowercaseurl.endsWith(".jsp") && !lowercaseurl.contains("login") && !lowercaseurl.contains("/ajax/"); 
 	}
 
 }

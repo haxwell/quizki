f846ff7d71e396e17b8e012e0c2ca8acd04b2b02
Johnathan <haxwell@gmail.com>
2014-04-24 20:56:09 -0600

    for debugging purposes, adding system.out.println

--- src/main/java/com/haxwell/apps/questions/events/runnables/ReloadLoggingPropertiesRunnable.java
+++ src/main/java/com/haxwell/apps/questions/events/runnables/ReloadLoggingPropertiesRunnable.java
@@ -14,7 +14,8 @@ public class ReloadLoggingPropertiesRunnable implements Runnable {
 		try {
 			LogManager.getLogManager().readConfiguration();
 			
-			log.log(Level.FINEST, "Reloaded logging configuration.");
+			//log.log(Level.FINEST, "Reloaded logging configuration.");
+			System.out.println("Reloaded logging configuration");
 		} catch (SecurityException | IOException e) {
 			e.printStackTrace();
 		}

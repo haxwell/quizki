795a08ea4666a5e739c707dfa13a72f99b2ae432
Johnathan <haxwell@gmail.com>
2014-05-11 15:56:30 -0600

    Added command to explicitly use quizki_db

--- src/main/resources/META-INF/sql/mysql/populate_database_quizkiDotCom_20130523_modified.sql
+++ src/main/resources/META-INF/sql/mysql/populate_database_quizkiDotCom_20130523_modified.sql
@@ -4,6 +4,8 @@
 -- ------------------------------------------------------
 -- Server version	5.1.69
 
+USE quizki_db;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;

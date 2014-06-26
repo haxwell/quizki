cc76ff3094132bd5b1d0571aa137d7afe5675e96
Johnathan <haxwell@gmail.com>
2014-06-26 08:19:04 -0600

    Initial commit BaseJWebUnitTest. Created because QuizkiTest and GenerateExamJSPTest both use common code.

---
+++ src/test/java/com/haxwell/apps/questions/BaseJWebUnitTest.java
@@ -0,0 +1,29 @@
+package com.haxwell.apps.questions;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
+import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
+import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
+
+import org.junit.Before;
+
+
+public class BaseJWebUnitTest {
+
+	@Before
+	public void prepare() {
+        setBaseUrl("http://localhost:8080/");
+    }
+	
+	public void beginAtHomePageTest()
+    {
+        beginAt("index.jsp");
+        assertTitleEquals("Home Page - Quizki");
+    }
+	
+	protected void assertAndClickOnLink(String linkId) {
+		assertLinkPresent(linkId);
+    	clickLink(linkId);
+	}
+
+}

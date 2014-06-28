fe854a3aa2c9d4a78aa4ccde933050266a718baa
Johnathan <haxwell@gmail.com>
2014-06-28 15:20:01 -0600

    Added marker interfaces to indicate the type of automated test. See JUnit 'Categories' and Maven's 'Surefire' and 'Failsafe' plugins.

---
+++ src/main/java/com/haxwell/apps/questions/testTypes/DatabaseTest.java
@@ -0,0 +1,5 @@
+package com.haxwell.apps.questions.testTypes;
+
+public interface DatabaseTest extends IntegrationTests {
+
+}
---
+++ src/main/java/com/haxwell/apps/questions/testTypes/UnitTests.java
@@ -0,0 +1,5 @@
+package com.haxwell.apps.questions.testTypes;
+
+public interface UnitTests {
+
+}

d4cd30cda50fb9cd48067a8055a5e0c1994f4fb8
Johnathan <haxwell@gmail.com>
2014-07-03 17:20:26 -0600

    polishing, added more test cases

--- src/test/java/com/haxwell/apps/questions/checkers/SingleQuestionTypeCheckerTest.java
+++ src/test/java/com/haxwell/apps/questions/checkers/SingleQuestionTypeCheckerTest.java
@@ -1,5 +1,8 @@
 package com.haxwell.apps.questions.checkers;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -11,7 +14,7 @@ import com.haxwell.apps.questions.constants.FilterConstants;
 import com.haxwell.apps.questions.constants.TypeConstants;
 import com.haxwell.apps.questions.entities.Choice;
 import com.haxwell.apps.questions.entities.Question;
-import com.haxwell.apps.questions.utils.QuestionGeneratorUtil;
+import com.haxwell.apps.questions.utils.QuestionAttributeSetterUtil;
 
 @Category(com.haxwell.apps.questions.testTypes.UnitTests.class) // think this may be a functional test
 public class SingleQuestionTypeCheckerTest {
@@ -21,13 +24,65 @@ public class SingleQuestionTypeCheckerTest {
 		Map<String, Object> attributes = new HashMap<>();
 		
 		attributes.put(FilterConstants.QUESTION_TYPE_FILTER, TypeConstants.SINGLE);
-		
-		Question q;
-		q = QuestionGeneratorUtil.getQuestion(attributes);
+
+		Question q = new Question();
+		q = QuestionAttributeSetterUtil.setQuestionAttributes(attributes, q);
 		
 		SingleQuestionTypeChecker sut = new SingleQuestionTypeChecker(q);
 		
-		Set<Choice> choices = q.getChoices();
+		assertTrue(sut.questionIsCorrect(getMapRepresentingCorrectChoices(q)));
+		assertFalse(sut.questionIsCorrect(getMapRepresentingASingleIncorrectlyChosenChoice(q)));
+		assertFalse(sut.questionIsCorrect(getMapRepresentingACorrectChoiceAndAnIncorrectChoice(q)));
+	}
+	
+	protected Map<String, String> getMapRepresentingCorrectChoices(Question q) {
+		Map<String, String> map = new HashMap<>();
 		
+		Set<Choice> set = q.getChoices();
+		
+		for (Choice c : set) {
+			if (c.getIscorrect() == Choice.CORRECT) {
+				map.put(q.getId() + "," + c.getId(), c.getText());
+			}
+		}
+		
+		return map;
+	}
+	
+	protected Map<String, String> getMapRepresentingASingleIncorrectlyChosenChoice(Question q) {
+		Map<String, String> map = new HashMap<>();
+		
+		Set<Choice> set = q.getChoices();
+		
+		for (Choice c : set) {
+			if (map.size() == 0 && c.getIscorrect() == Choice.NOT_CORRECT) {
+				map.put(q.getId() + "," + c.getId(), c.getText());
+			}
+		}
+		
+		return map;
+	}
+	
+	protected Map<String, String> getMapRepresentingACorrectChoiceAndAnIncorrectChoice(Question q) {
+		Map<String, String> map = new HashMap<>();
+		
+		Set<Choice> set = q.getChoices();
+		
+		boolean correctAdded = false;
+		boolean incorrectAdded = false;
+		
+		for (Choice c : set) {
+			if (!correctAdded && c.getIscorrect() == Choice.CORRECT) {
+				map.put(q.getId() + "," + c.getId(), c.getText());
+				correctAdded = true;
+			}
+			
+			if (!incorrectAdded && c.getIscorrect() == Choice.NOT_CORRECT) {
+				map.put(q.getId() + "," + c.getId(), c.getText());
+				incorrectAdded = true;
+			}
+		}
+		
+		return map;
 	}
 }

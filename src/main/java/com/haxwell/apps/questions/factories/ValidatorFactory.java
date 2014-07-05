2778c03aa6d756816cfede98b414deb35cd3ef8b
Johnathan <haxwell@gmail.com>
2014-07-05 13:31:47 -0600

    Added the QuestionValidator

--- src/main/java/com/haxwell/apps/questions/factories/ValidatorFactory.java
+++ src/main/java/com/haxwell/apps/questions/factories/ValidatorFactory.java
@@ -2,7 +2,9 @@ package com.haxwell.apps.questions.factories;
 
 import com.haxwell.apps.questions.entities.AbstractEntity;
 import com.haxwell.apps.questions.entities.Exam;
+import com.haxwell.apps.questions.entities.Question;
 import com.haxwell.apps.questions.validators.ExamValidator;
+import com.haxwell.apps.questions.validators.QuestionValidator;
 import com.haxwell.apps.questions.validators.Validator;
 
 public class ValidatorFactory {
@@ -10,6 +12,7 @@ public class ValidatorFactory {
 	public static Validator getValidator(AbstractEntity e) {
 		
 		if (e instanceof Exam) return new ExamValidator();
+		if (e instanceof Question) return new QuestionValidator();
 		
 		return null;
 	}

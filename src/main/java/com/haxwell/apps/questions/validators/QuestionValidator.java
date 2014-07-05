c954a1cb17deb26e066142b45a36912184c2e996
Johnathan <haxwell@gmail.com>
2014-07-05 13:31:03 -0600

    Removed implementation from QuestionManager, and set it here as it should be, so this could be called by ValidatorUtil

--- src/main/java/com/haxwell/apps/questions/validators/QuestionValidator.java
+++ src/main/java/com/haxwell/apps/questions/validators/QuestionValidator.java
@@ -1,34 +1,56 @@
 package com.haxwell.apps.questions.validators;
 
-import java.util.ArrayList;
+import java.util.ArrayList;	
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.haxwell.apps.questions.constants.Constants;
 import com.haxwell.apps.questions.entities.AbstractEntity;
-import com.haxwell.apps.questions.entities.Choice;
 import com.haxwell.apps.questions.entities.Question;
+import com.haxwell.apps.questions.entities.Reference;
+import com.haxwell.apps.questions.entities.Topic;
+import com.haxwell.apps.questions.managers.ChoiceManager;
 import com.haxwell.apps.questions.utils.StringUtil;
 
 public class QuestionValidator implements Validator {
 
-	@Override
-	public boolean validate(AbstractEntity e, Map<String, List<String>> errors) {
-		boolean rtn = true;
-		Question q = (Question)e;
+	public boolean validate(AbstractEntity q, Map<String, List<String>> errors) {
+		Question questionObj = (Question)q;
 		List<String> errorsList = new ArrayList<String>();
+
+		errorsList.addAll(ChoiceManager.validate(questionObj));
 		
-		Set<Choice> choices = q.getChoices();
+		String questionText = questionObj.getText();
+		Set<Topic> topics = questionObj.getTopics();
 		
-		if (choices.size() == 0) {
-			errorsList.add("No choices have been added!");
-			rtn = false;
+		if (topics.size() < 1)
+			errorsList.add("Question must have at least one topic.");
+		
+		for (Topic t : topics) {
+			if (t.getText().indexOf("\"") != -1)
+				errorsList.add("Topics cannot have quotes in them. Why not try an apostrophe instead?");
 		}
 		
-		if (!rtn)
-			errors.put("examValidationErrors", errorsList);
+		Set<Reference> references = questionObj.getReferences();
 		
-		return rtn;
+		for (Reference r : references) {
+			if (r.getText().indexOf("\"") != -1)
+				errorsList.add("References cannot have quotes in them. Why not try an apostrophe instead?");
+		}
+		
+		if (StringUtil.isNullOrEmpty(questionText))
+			errorsList.add("Question must have some text!");
+		
+		if (questionText != null && questionText.length() > Constants.MAX_QUESTION_TEXT_LENGTH)
+			errorsList.add("Question text cannot be longer than " + Constants.MAX_QUESTION_TEXT_LENGTH + " characters. Perhaps a seperate question is in order!");
+		
+		if (questionObj.getDescription() != null && questionObj.getDescription().length() > Constants.MAX_QUESTION_DESCRIPTION_LENGTH)
+			errorsList.add("Question description cannot be longer than " + Constants.MAX_QUESTION_DESCRIPTION_LENGTH + " characters.");
+		
+		if (errorsList.size() > 0)
+			errors.put("questionValidationErrors", errorsList);
+		
+		return errorsList.size() > 0;
 	}
-
 }

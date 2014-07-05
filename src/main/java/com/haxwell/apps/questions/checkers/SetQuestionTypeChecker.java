9aca2d328d3ac7167f946b9151ce6d60db5c4c1f
Johnathan <haxwell@gmail.com>
2014-07-05 17:32:41 -0600

    corrected a bug in which if the field num was -1, it wasn't using the choice text.. made the constructor call super, as its better for test coverage'

--- src/main/java/com/haxwell/apps/questions/checkers/SetQuestionTypeChecker.java
+++ src/main/java/com/haxwell/apps/questions/checkers/SetQuestionTypeChecker.java
@@ -12,7 +12,7 @@ import com.haxwell.apps.questions.utils.StringUtil;
 public class SetQuestionTypeChecker extends AbstractQuestionTypeChecker {
 
 	public SetQuestionTypeChecker(Question q) {
-		this.question = q;
+		super(q);
 	}
 		
 	@Override
@@ -22,15 +22,23 @@ public class SetQuestionTypeChecker extends AbstractQuestionTypeChecker {
 		
 		boolean rtn = true;
 		List<Choice> choices = QuestionUtil.getChoiceList(this.question);
+		String dynamicData = (String)this.question.getDynamicData("choiceIdsToBeAnswered");
+		String fieldNum = StringUtil.getField(2, ";", ";", dynamicData);
 
 		for (Choice c : choices)
 		{
 			long choiceId = c.getId();
-			String dynamicData = (String)this.question.getDynamicData("choiceIdsToBeAnswered");
-			String fieldNum = StringUtil.getField(2, ";", ";", dynamicData);
 
 			if (selectedChoiceIds.contains(choiceId+","+fieldNum)) {
-				String text = StringUtil.getField(Integer.parseInt(fieldNum), "[[", "]]", c.getText());
+				String text = null;
+				
+				if (StringUtil.equals(fieldNum, "-1")) {
+					text = c.getText();
+				}
+				else {
+					text = StringUtil.getField(Integer.parseInt(fieldNum), "[[", "]]", c.getText());
+				}
+
 				rtn &= mapOfFieldNamesToValues.get(this.question.getId() + "," + choiceId + "," + fieldNum).equals(text);
 			}
 		}

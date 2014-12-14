a5e719e4147dc6825dd1389f1b64d969f0bc76c1
Johnathan <haxwell@gmail.com>
2014-12-14 15:14:07 -0700

    polishing the install scripts..

--- install-droplet-post.sh
+++ install-droplet-post.sh
@@ -4,9 +4,12 @@ mkdir ~/apps/apache-shiro
 git clone https://github.com/haxwell/apache-shiro-1.2.3.git
 cd apache-shiro-1.2.3
 git checkout 1.2.x
-mvn clean package -DskipTests
+mvn clean install -DskipTests
 echo
+echo --------------------------------------------------------------------------
 echo Expected a failure above.. as long as CORE and WEB built, Quizki can build
+echo --------------------------------------------------------------------------
+echo
 
 cd ~/apps/quizki
 mvn clean package

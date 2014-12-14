ec748e74117963143470c9212a20f4cac6836efc
Johnathan <haxwell@gmail.com>
2014-12-14 16:02:33 -0700

    Copy to Tomcat, create settings file for Tomcat, start Tomcat

--- install-droplet-post.sh
+++ install-droplet-post.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# 
+# This script should be run as sudo
+#
+if [ "$(id -u)" != 0 ]; then
+    echo "You should run this as root."
+    exit 1
+fi
+
 mkdir ~/apps/apache-shiro
 git clone https://github.com/haxwell/apache-shiro-1.2.3.git
 cd apache-shiro-1.2.3
@@ -13,3 +21,16 @@ echo
 
 cd ~/apps/quizki
 mvn clean package
+
+echo ----------------------
+echo setting up Tomcat
+echo ----------------------
+
+bash -c 'echo "#! /bin/sh" >> /home/quizki/apps/apache-tomcat-7.0.57/bin/setenv.sh'
+bash -c 'echo export JAVA_OPTS=\"$JAVA_OPTS -DQUIZKI_JDBC_URL=jdbc:mysql://localhost:3306/quizki_db -DSHIRO_SERVER_NAME=localhost\" >> /home/quizki/apps/apache-tomcat-7.0.57/bin/setenv.sh'
+
+rm $TOMCAT_HOME/webapps/ROOT -rf
+cp target/quizki-1.3.war $TOMCAT_HOME/webapps/ROOT.war
+
+cd $TOMCAT_HOME
+./bin/catalina.sh start

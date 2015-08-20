43d94f5be25d12d1e5f381a62074dfef20e16fb6
Johnathan <haxwell@gmail.com>
2015-08-20 12:06:09 -0600

    comments

--- install-droplet-post.sh
+++ install-droplet-post.sh
@@ -14,9 +14,9 @@ cd apache-shiro-1.2.3
 git checkout 1.2.x
 mvn clean install -DskipTests
 echo
-echo --------------------------------------------------------------------------
-echo Expected a failure above.. as long as CORE and WEB built, Quizki can build
-echo --------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------
+echo Expected a failure above.. as long as Shiro CORE and Shiro WEB built, Quizki can build
+echo --------------------------------------------------------------------------------------
 echo
 
 cd ~/apps/quizki

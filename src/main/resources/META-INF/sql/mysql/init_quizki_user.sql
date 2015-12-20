cbeba19099cc915bb6eb97d4f156f3c3c56729da
Johnathan <haxwell@gmail.com>
2015-12-20 16:06:11 -0700

    added commands to drop the database and user if they are present, so when the script is run again, it won't error out.

--- src/main/resources/META-INF/sql/mysql/init_quizki_user.sql
+++ src/main/resources/META-INF/sql/mysql/init_quizki_user.sql
@@ -1,3 +1,5 @@
+DROP USER 'quizki'@'localhost';
 CREATE USER 'quizki'@'localhost' IDENTIFIED BY 'quizki';
+DROP DATABASE quizki_db;
 CREATE DATABASE quizki_db;
 GRANT ALL ON quizki_db.* TO 'quizki'@'localhost';
